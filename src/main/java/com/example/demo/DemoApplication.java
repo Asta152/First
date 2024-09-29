ec17990f3e6a118cd8b757f885c2d179d2008478
Dastan <astadavidov@gmail.com>
2024-09-29 21:46:18 +0600

    copy past

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,11 +15,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping
-	public List<Bok> hello(){
-		return List.of(
-				new Bok(1,"Elden RIng",500,39,"Miyazaki" ),
-				new Bok(2,"Bloodborne",300,29,"Miyazaki" )
-		);
+
 	}
-}
+
---
+++ src/main/java/com/example/demo/books/BookService.java
@@ -0,0 +1,27 @@
+package com.example.demo.books;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    public List<Bok> bookList(){
+        return List.of(
+                new Bok(
+                        1,
+                        "1984",
+                        270,
+                        300,
+                        "J.Oruel"
+                ),
+                new Bok(
+                        2,
+                        "Горжость и предубеждение",
+                        383,
+                        400,
+                        "Jane Osten"
+                )
+        );
+    }
+}
